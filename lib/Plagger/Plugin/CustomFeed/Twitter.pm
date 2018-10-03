7ccfb3f1924e96689fa99053070d7adce7e2a1d8
Fujii Hironori <fujii.hironori@gmail.com>
2018-10-03 21:28:09 +0900

    Twitter.pm: Hook customfeed.handle instead of subscription.load

--- lib/Plagger/Plugin/CustomFeed/Twitter.pm
+++ lib/Plagger/Plugin/CustomFeed/Twitter.pm
@@ -10,65 +10,70 @@ sub register {
     my($self, $context) = @_;
     $context->register_hook(
         $self,
-        'subscription.load' => $self->can('load'),
-        );
+        'customfeed.handle' => \&handle,
+    );
 }
 
-sub load {
-    my($self, $context) = @_;
-    
-    my $feed = Plagger::Feed->new;
-    $feed->aggregator(sub { $self->aggregate(@_) });
-    $context->subscription->add($feed);
+sub handle {
+    my($self, $context, $args) = @_;
+
+    if ($args->{feed}->url =~ m!^https?://twitter\.com/!) {
+        return $self->aggregate($context, $args);
+    }
+
+    return;
 }
 
 sub aggregate {
     my ($self, $context, $args) = @_;
 
-    foreach my $url (@{$self->conf->{uri}}) {
-	$context->log(info => $url);
-	my $agent = Plagger::UserAgent->new;
+    my $url = $args->{feed}->url;
+    $context->log(info => $url);
+    my $agent = Plagger::UserAgent->new;
 
-	my $res = $agent->fetch($url, $self);
-	if ($res->http_response->is_error) {
-	    $context->log(error => "GET $url failed: " . $res->status);
-	    return;
-	}
-	my $html = decode_content($res);
-	my $entry = scraper {
-	    process 'p.tweet-text', post => 'HTML';
-	    process 'a.tweet-timestamp', url => '@href';
-	    process 'a.tweet-timestamp>span', date => '@data-time';
-	    process 'span.username>b', id => 'TEXT';
-	};
-	my $res = scraper {
-	    process 'title', 'title' => 'TEXT';
-	    process 'div.content', 'entry[]' => $entry;
-	}->scrape($html);
-
-	my $feed = Plagger::Feed->new;
-	$feed->type('twitter');
-	$feed->link($url);
-	$feed->title($res->{title});
-
-	foreach my $line (@{$res->{entry}}){
-	    my $entry  = Plagger::Entry->new;
-	    my $id = $line->{id};
-	    my $post = $line->{post};
-	    my $title = substr(Plagger::Util::strip_html($post), 0, 100);
-
-	    $context->log(debug => $post);
-	    my $dt = eval { Plagger::Date->from_epoch($line->{date}) };
-	    $entry->date($dt) if $dt;
-	    $entry->body($post);
-	    $entry->author($id);
-	    $entry->title($title);
-	    $entry->link(URI->new_abs($line->{url}, $url));
-	    
-	    $feed->add_entry($entry);
-	}
-	$context->update->add($feed);
+    my $res = $agent->fetch($url, $self);
+    if ($res->http_response->is_error) {
+	$context->log(error => "GET $url failed: " . $res->status);
+	return;
     }
+    my $html = decode_content($res);
+    my $entry = scraper {
+	process 'p.tweet-text', post => 'HTML';
+	process 'a.tweet-timestamp', url => '@href';
+	process 'a.tweet-timestamp>span', date => '@data-time';
+	process 'span.username>b', username => 'TEXT';
+    };
+    my $res = scraper {
+	process 'title', 'title' => 'TEXT';
+	process 'div.content', 'entry[]' => $entry;
+    }->scrape($html);
+
+    my $feed = Plagger::Feed->new;
+    $feed->type('twitter');
+    $feed->link($url);
+    $feed->title($res->{title});
+
+    foreach my $line (@{$res->{entry}}){
+	my $entry  = Plagger::Entry->new;
+	my $username = $line->{username};
+	my $post = $line->{post};
+	my $title = substr(Plagger::Util::strip_html($post), 0, 100);
+
+	$context->log(debug => "username: " . $username);
+	$context->log(debug => "post: " . $post);
+	$context->log(debug => "title: " . $title);
+
+	my $dt = eval { Plagger::Date->from_epoch($line->{date}) };
+	$entry->date($dt) if $dt;
+	$entry->body($post);
+	$entry->author($username);
+	$entry->title($title);
+	$entry->link(URI->new_abs($line->{url}, $url));
+	
+	$feed->add_entry($entry);
+    }
+    $context->update->add($feed);
+    return 1;
 }
 
 1;
@@ -81,12 +86,14 @@ Plagger::Plugin::CustomFeed::Twitter - Scraping Twitter HTML.
 
 =head1 SYNOPSIS
 
-  - module: CustomFeed::Twitter
+  - module: Subscription::Config
     config:
-       uri:
+      feed:
           - https://twitter.com/riywo
           - https://twitter.com/fujii0
           - https://twitter.com/search?q=emacs%20min_faves%3A1
+
+  - module: CustomFeed::Twitter
 
 =head1 DESCRIPTION
 
